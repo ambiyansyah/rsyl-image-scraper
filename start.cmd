16ba0226b304bfce87dff2da9ad8c80d387df666
Risyal Ambiyansyah <Risyal_A916@mitrais.com>
2019-10-02 14:16:08 +0700

    change keywords in start.cmd as sample execution

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
-node index.js --keyword="microservice architecture" --limit=100
-node index.js --keyword="microservices" --limit=100
-node index.js --keyword="golang microservices" --limit=100
+node index.js --keyword="home design" --limit=100
+node index.js --keyword="joker movie" --limit=100
+node index.js --keyword="nature" --limit=100
