63ee8c01be5a5324824eb0db3682545f554d186f
Risyal Ambiyansyah <Risyal_A916@mitrais.com>
2019-10-02 14:13:31 +0700

    remove fs package and use fs-extra to use mkdirp command in sync mode

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
 "use strict";
 
-const Scraper = require('./lib/google-images-scraper');
-const argv = require('yargs').argv;
+const Scraper   = require('./lib/google-images-scraper');
+const argv      = require('yargs').argv;
 const UserAgent = require('user-agents');
-const download = require('image-downloader');
-const fs = require('fs');
+const download  = require('image-downloader');
+const fs        = require('fs-extra');
 
 let userAgent = new UserAgent();
-let keyword = argv.keyword.replace(/[^a-zA-Z ]/g, '').trim().replace(/\s\s+/g, ' ').toLowerCase();
-let google = new Scraper.Google({
-    keyword: keyword,
+let keyword   = argv.keyword.replace(/[^a-zA-Z ]/g, '').trim().replace(/\s\s+/g, ' ').toLowerCase();
+let google    = new Scraper.Google({
+    keyword  : keyword,
     userAgent: userAgent.toString(),
-    limit: argv.limit,
+    limit    : argv.limit,
     puppeteer: {
         headless: false
     },
     advanced: {
-        imgType: 'photo',   // options: clipart, face, lineart, news, photo
+        imgType   : 'photo',   // options: clipart, face, lineart, news, photo
         resolution: 'l',       // options: l(arge), m(edium), i(cons), etc.
-        color: undefined  // options: color, gray, trans
+        color     : undefined  // options: color, gray, trans
     }
 });
 
 const downloadImages = async (results) => {
-    const dir = `./images/${keyword.replace(/\s+/g, '-')}`;
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-
+    let dir            = `./images/${keyword.replace(/\s+/g, '-')}`;
     let downloadedFile = [];
+    fs.ensureDirSync(dir)
 
     for (let result of results) {
-
         if (result.type) {
-            let imgTitle = result.title.replace(/[^a-zA-Z ]/g, '').trim().replace(/\s\s+/g, ' ');
-            let imgAlt = imgTitle.replace(/\s+/g, '-').toLowerCase();
+            let imgTitle    = result.title.replace(/[^a-zA-Z ]/g, '').trim().replace(/\s\s+/g, ' ');
+            let imgAlt      = imgTitle.replace(/\s+/g, '-').toLowerCase();
             let imgFilename = imgAlt + '.' + result.type;
 
             const options = {
-                url: result.url,
-                dest: `${dir}/${imgFilename}`,
+                url    : result.url,
+                dest   : `${dir}/${imgFilename}`,
                 headers: {
                     'User-Agent': userAgent.toString()
                 }
@@ -59,11 +55,7 @@ const downloadImages = async (results) => {
 }
 
 (async () => {
-    console.log(`searching image links with keyword: ${keyword}`);
-    const results = await google.start();      // search images link base on keyword given
-    console.log(`found ${results.length} image links`);
-    
-    console.log(`start download images`);
+    const results  = await google.start();
     const download = await downloadImages(results);  // start to download images
 
     console.log('results', download.length);
