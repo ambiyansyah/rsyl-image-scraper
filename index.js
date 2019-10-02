fb826c661f71ad8a1e4431a05e1386bc0c10264f
Risyal Ambiyansyah <Risyal_A916@mitrais.com>
2019-10-02 13:10:45 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const Scraper = require('./lib/google-images-scraper');
+const argv = require('yargs').argv;
+const UserAgent = require('user-agents');
+const download = require('image-downloader');
+const fs = require('fs');
+
+let userAgent = new UserAgent();
+let keyword = argv.keyword.replace(/[^a-zA-Z ]/g, '').trim().replace(/\s\s+/g, ' ').toLowerCase();
+let google = new Scraper.Google({
+    keyword: keyword,
+    userAgent: userAgent.toString(),
+    limit: argv.limit,
+    puppeteer: {
+        headless: true
+    },
+    advanced: {
+        imgType: 'photo',   // options: clipart, face, lineart, news, photo
+        resolution: 'l',       // options: l(arge), m(edium), i(cons), etc.
+        color: undefined  // options: color, gray, trans
+    }
+});
+
+const downloadImages = async (results) => {
+    const dir = `./images/${keyword.replace(/\s+/g, '-')}`;
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+
+    let downloadedFile = [];
+
+    for (let result of results) {
+
+        if (result.type) {
+            let imgTitle = result.title.replace(/[^a-zA-Z ]/g, '').trim().replace(/\s\s+/g, ' ');
+            let imgAlt = imgTitle.replace(/\s+/g, '-').toLowerCase();
+            let imgFilename = imgAlt + '.' + result.type;
+
+            const options = {
+                url: result.url,
+                dest: `${dir}/${imgFilename}`
+            }
+
+            try {
+                console.log(`download ${result.url}`);
+                const { filename, image } = await download.image(options)
+                downloadedFile.push(filename)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+
+    return downloadedFile;
+}
+
+(async () => {
+    const results = await google.start();      // search images link base on keyword given
+    const download = await downloadImages(results);  // start to download images
+
+    console.log('results', download.length);
+})();
---
+++ start.cmd
@@ -0,0 +1,3 @@
+node index.js --keyword="microservice architecture" --limit=100
+node index.js --keyword="microservices" --limit=100
+node index.js --keyword="golang microservices" --limit=100
